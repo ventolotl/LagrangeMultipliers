dd1ed6c3cc15aa3111a7d6aa80bef0ae216afc95
ventolotl <ventolotl@protonmail.com>
2024-01-03 20:11:08 +0100

    small refactor

--- src/main/kotlin/de/ventolotl/lagrange/maths/Constraint.kt
+++ src/main/kotlin/de/ventolotl/lagrange/maths/Constraint.kt
@@ -2,17 +2,23 @@ package de.ventolotl.lagrange.maths
 
 import de.ventolotl.lagrange.maths.rootfinder.findRootsNewton
 import de.ventolotl.lagrange.utility.DoubleVector2Range
+import de.ventolotl.lagrange.utility.Point2d
 
 data class Constraint(
     val equation: Function3d,
     val constant: Double,
     val range: DoubleVector2Range,
-    val accuracy: Double
+    val step: Double
 ) {
-    val points by lazy {
+    val points = calculatePoints()
+
+    private fun calculatePoints(): List<Point2d> {
         val correspondingFunction = Function3d { x, y ->
             equation.eval(x, y) - constant
         }
-        correspondingFunction.findRootsNewton(range, accuracy)
+        println("Finding points")
+        return correspondingFunction.findRootsNewton(range, step).also {
+            println("Finished")
+        }
     }
 }
--- src/main/kotlin/de/ventolotl/lagrange/maths/ContourPlot.kt
+++ src/main/kotlin/de/ventolotl/lagrange/maths/ContourPlot.kt
@@ -3,15 +3,16 @@ package de.ventolotl.lagrange.maths
 import de.ventolotl.lagrange.maths.rootfinder.findRootsNewton
 import de.ventolotl.lagrange.utility.DoubleRange
 import de.ventolotl.lagrange.utility.DoubleVector2Range
+import de.ventolotl.lagrange.utility.Point2d
 import kotlin.math.absoluteValue
 
-open class ContourLine(val z: Double, val points: List<Vector2d<Double>>)
+open class ContourLine(val z: Double, val points: List<Point2d>)
 
 fun Function3d.createContour(
     zRange: DoubleRange,
     zAccuracy: Double,
     pointsRange: DoubleVector2Range,
-    pointsAccuracy: Double
+    pointsStep: Double
 ): List<ContourLine> {
     val contour = mutableListOf<ContourLine>()
     var iteration = 0
@@ -19,7 +20,7 @@ fun Function3d.createContour(
     zRange.iterate(zAccuracy) { z ->
         val correspondingFunction = Function3d { x, y -> eval(x, y) - z }
 
-        val points = correspondingFunction.findRootsNewton(pointsRange, pointsAccuracy)
+        val points = correspondingFunction.findRootsNewton(pointsRange, pointsStep)
         contour.add(ContourLine(z, points))
 
         val completedPct = 100.0 * ++iteration / (zRange.start.absoluteValue + zRange.end.absoluteValue + 1)
