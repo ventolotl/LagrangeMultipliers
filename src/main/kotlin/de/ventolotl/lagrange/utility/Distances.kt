24db7eaca79678a338130501384a49b08aafc000
ventolotl <ventolotl@protonmail.com>
2024-01-12 22:25:14 +0100

    fix distance calculation

--- src/main/kotlin/de/ventolotl/lagrange/utility/Distances.kt
+++ src/main/kotlin/de/ventolotl/lagrange/utility/Distances.kt
@@ -6,11 +6,11 @@ import kotlin.math.sqrt
 fun <T : Number> Vector2d<T>.dist(other: Vector2d<T>): Double {
     val deltaX = (this.x - other.x).toDouble()
     val deltaY = (this.y - other.y).toDouble()
-    return sqrt(deltaX * deltaX + deltaY + deltaY)
+    return sqrt(deltaX * deltaX + deltaY * deltaY)
 }
 
 fun <T : Number> Vector2d<T>.distSq(other: Vector2d<T>): Double {
     val deltaX = (this.x - other.x).toDouble()
     val deltaY = (this.y - other.y).toDouble()
-    return deltaX * deltaX + deltaY + deltaY
+    return deltaX * deltaX + deltaY * deltaY
 }
