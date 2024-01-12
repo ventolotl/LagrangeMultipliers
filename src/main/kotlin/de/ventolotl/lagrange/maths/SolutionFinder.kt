ef40ca19de4cdafe2539dd3befde698cfbe72845
ventolotl <ventolotl@protonmail.com>
2024-01-12 22:24:17 +0100

    improve SolutionFinder.kt

--- src/main/kotlin/de/ventolotl/lagrange/maths/SolutionFinder.kt
+++ src/main/kotlin/de/ventolotl/lagrange/maths/SolutionFinder.kt
@@ -8,24 +8,36 @@ fun Function3d.optimize(constraint: Constraint): List<Point2d> {
     return solutionFinder.findSolutions()
 }
 
+private const val PRECISION = 0.005
+
 private class SolutionFinder(
-    function3d: Function3d,
+    functionToOptimize: Function3d,
     private val constraint: Constraint
 ) {
-    private val gradientFunction = function3d.gradient()
+    private val constraintEq = constraint.equation
+
+    private val gradientOptimizeFunc = functionToOptimize.gradient()
     private val gradientConstraint = constraint.equation.gradient()
 
     fun findSolutions(): List<Vector2d<Double>> {
-        return errorFunction().findRootsNewton(constraint.range, 0.1)
+        val roots = errorFunction().findRootsNewton(constraint.range, PRECISION)
+        return filterFunction(roots)
+    }
+
+    private fun filterFunction(roots: List<Vector2d<Double>>): List<Vector2d<Double>> {
+        return roots.filter { root ->
+            val delta = constraintEq.eval(root.x, root.y) - constraint.constant
+            delta * delta < PRECISION
+        }
     }
 
     private fun errorFunction(): Function3d {
         return Function3d { x, y ->
-            val lambda1 = gradientFunction.x.eval(x, y) / gradientConstraint.x.eval(x, y)
-            val lambda2 = gradientFunction.y.eval(x, y) / gradientConstraint.y.eval(x, y)
+            val lambda1 = gradientOptimizeFunc.x.eval(x, y) / gradientConstraint.x.eval(x, y)
+            val lambda2 = gradientOptimizeFunc.y.eval(x, y) / gradientConstraint.y.eval(x, y)
 
             val error = lambda1 - lambda2
-            error
+            error * error
         }
     }
 }
