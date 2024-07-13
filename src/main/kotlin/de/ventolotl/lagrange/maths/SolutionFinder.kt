be2b0e65771f47eaca1639115c2c5d8136d6d7d9
ventolotl <ventolotl@protonmail.com>
2024-07-13 16:07:50 +0200

    cleanup SolutionFinder code

--- src/main/kotlin/de/ventolotl/lagrange/maths/SolutionFinder.kt
+++ src/main/kotlin/de/ventolotl/lagrange/maths/SolutionFinder.kt
@@ -6,48 +6,37 @@ import de.ventolotl.lagrange.utility.distSq
 import de.ventolotl.lagrange.utility.iterate
 
 fun Function3d.optimize(constraint: Constraint): List<Vector2d<Double>> {
-    val solutionFinder = SolutionFinder(this, constraint)
-    return solutionFinder.findSolutions()
-}
+    val constraintEq = constraint.rootFunction
 
-private class SolutionFinder(
-    private val functionToOptimize: Function3d, private val constraint: Constraint
-) {
-    private val constraintEq = constraint.rootFunction
+    val functionToOptimizePartialX = this.partialX()
+    val functionToOptimizePartialY = this.partialY()
 
-    fun findSolutions(): List<Vector2d<Double>> {
-        val functionToOptimizePartialX = functionToOptimize.partialX()
-        val functionToOptimizePartialY = functionToOptimize.partialY()
+    val constraintPartialX = constraintEq.partialX()
+    val constraintPartialY = constraintEq.partialY()
 
-        val constraintPartialX = constraintEq.partialX()
-        val constraintPartialY = constraintEq.partialY()
+    val f1 = Function4d { x, y, lambda ->
+        functionToOptimizePartialX.eval(x, y) - lambda * constraintPartialX.eval(x, y)
+    }
+    val f2 = Function4d { x, y, lambda ->
+        functionToOptimizePartialY.eval(x, y) - lambda * constraintPartialY.eval(x, y)
+    }
+    val g = Function4d { x, y, _ -> constraintEq.eval(x, y) }
 
-        val f1 = Function4d { x, y, lambda ->
-            functionToOptimizePartialX.eval(x, y) - lambda * constraintPartialX.eval(x, y)
-        }
-        val f2 = Function4d { x, y, lambda ->
-            functionToOptimizePartialY.eval(x, y) - lambda * constraintPartialY.eval(x, y)
-        }
-        val g = Function4d { x, y, _ -> constraintEq.eval(x, y) }
+    val equationSolver = EquationSolver(f1, f2, g)
 
-        val equationSolver = EquationSolver(f1, f2, g, 1000)
+    val solutions = mutableListOf<Vector3d<Double>>()
 
-        val solutions = mutableListOf<Vector2d<Double>>()
-
-        constraint.range.iterate(0.5) { x, y ->
-            val solution = equationSolver.findSolution(Vector3d(x, y, 1.0)) ?: run {
-                return@iterate
-            }
-            val coordinates = Vector2d(solution.x, solution.y)
+    constraint.range.iterate(0.05) { x, y ->
+        equationSolver.findSolution(Vector3d(x, y, 1.0))?.let { solution ->
             val unknown = solutions.none { other ->
-                other.distSq(coordinates) < 1e-5
+                other.distSq(solution) < 0.01
             }
 
             if (unknown) {
-                solutions.add(coordinates)
+                solutions.add(solution)
             }
         }
-
-        return solutions
     }
+
+    return solutions.map { (x, y, _) -> Vector2d(x, y) }
 }
