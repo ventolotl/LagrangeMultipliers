cc768ab63e5e2679c0c7e65660ed190e8e98f7f7
ventolotl <ventolotl@protonmail.com>
2024-07-13 16:07:18 +0200

    connect nearby optimization solutions

--- src/main/kotlin/de/ventolotl/lagrange/ui/fragments/ContourPane.kt
+++ src/main/kotlin/de/ventolotl/lagrange/ui/fragments/ContourPane.kt
@@ -5,6 +5,8 @@ import de.ventolotl.lagrange.ui.LagrangePane
 import de.ventolotl.lagrange.ui.fillOval
 import de.ventolotl.lagrange.ui.utility.FunctionRenderer
 import de.ventolotl.lagrange.ui.write
+import de.ventolotl.lagrange.utility.Vector2d
+import de.ventolotl.lagrange.utility.distSq
 import javafx.scene.paint.Color
 import javafx.scene.text.Font
 import kotlin.math.max
@@ -16,11 +18,12 @@ class ContourPane(lagrangePane: LagrangePane, private val grid: GridPane) : UIFr
     private val constraint = lagrangePane.constraint
 
     private val solutions = function3d.optimize(constraint)
+    private val connectedSolutions = FunctionRenderer.computeAlgebraicConnections(solutions, 0.2)
 
     private val contourConnections = lagrangePane.contourLines.associateWith {
-        FunctionRenderer.computeAlgebraicConnections(it.points)
+        FunctionRenderer.computeAlgebraicConnections(it.points, 1.0)
     }
-    private val constraintConnections = FunctionRenderer.computeAlgebraicConnections(constraint.points)
+    private val constraintConnections = FunctionRenderer.computeAlgebraicConnections(constraint.points, 1.0)
 
     override fun paint() {
         val width = max(canvas.width, canvas.height) * 0.02
@@ -30,14 +33,37 @@ class ContourPane(lagrangePane: LagrangePane, private val grid: GridPane) : UIFr
         }
         FunctionRenderer.renderGraph(grid, ctx, constraintConnections, constraint.color, 4.0)
 
+        paintSolutions()
+    }
+
+    private fun paintSolutions() {
+        val width = 10.0
+
+        // Draw a circle
         solutions.forEach { solution ->
             val windowPoint = grid.algebraicToWindowCoordinates(solution)
-            ctx.fillOval(windowPoint, radius = 15.0, Color.WHITE)
+            ctx.fillOval(windowPoint, width, Color.WHITE)
+        }
 
+        // Write the coordinates of the solution
+        val visualizedSolutions = mutableListOf<Vector2d<Double>>()
+        solutions.forEach { solution ->
+            val solutionNearby = visualizedSolutions.any { other ->
+                other.distSq(solution) < 0.1
+            }
+            if (solutionNearby) {
+                return@forEach
+            }
+
+            visualizedSolutions.add(solution)
+            val windowPoint = grid.algebraicToWindowCoordinates(solution)
             val text = "(%.3f, %.3f) at z=%.3f".format(
                 solution.x, solution.y, function3d.eval(solution.x, solution.y)
             )
             ctx.write(text, windowPoint, Color.WHITE, contourFont)
         }
+
+        // Connect nearby solutions
+        FunctionRenderer.renderGraph(grid, ctx, connectedSolutions, Color.WHITE, width)
     }
 }
--- src/main/kotlin/de/ventolotl/lagrange/ui/utility/FunctionRenderer.kt
+++ src/main/kotlin/de/ventolotl/lagrange/ui/utility/FunctionRenderer.kt
@@ -8,12 +8,11 @@ import javafx.scene.canvas.GraphicsContext
 import javafx.scene.paint.Color
 
 internal object FunctionRenderer {
-    private const val DIST_TO_CONNECT = 1.0
-    private const val DIST_TO_CONNECT_SQ = DIST_TO_CONNECT * DIST_TO_CONNECT
-
     fun computeAlgebraicConnections(
-        algebraicCoordinates: List<Vector2d<Double>>
+        algebraicCoordinates: List<Vector2d<Double>>,
+        distToConnect: Double
     ): List<Pair<Vector2d<Double>, Vector2d<Double>>> {
+        val distToConnectSq = distToConnect * distToConnect
         val remainingAlgebraicCoords = algebraicCoordinates.toMutableList()
 
         val first = remainingAlgebraicCoords.firstOrNull() ?: return emptyList()
@@ -26,14 +25,14 @@ internal object FunctionRenderer {
                 point.distSq(lastPoint)
             }
 
-            if (nearest.distSq(lastPoint) < DIST_TO_CONNECT_SQ) {
+            if (nearest.distSq(lastPoint) < distToConnectSq) {
                 list.add(nearest to lastPoint)
             }
             lastPoint = nearest
             remainingAlgebraicCoords.remove(nearest)
         }
 
-        if (first.distSq(lastPoint) < DIST_TO_CONNECT_SQ) {
+        if (first.distSq(lastPoint) < distToConnectSq) {
             list.add(first to lastPoint)
         }
 
