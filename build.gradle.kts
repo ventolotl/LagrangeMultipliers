a474fa9ba4e81670982ea12737c7c38d25f896da
ventolotl <ventolotl@protonmail.com>
2024-07-15 23:32:45 +0200

    update javafxplugin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.22"
     id("application")
-    id("org.openjfx.javafxplugin") version "0.0.8"
+    id("org.openjfx.javafxplugin") version "0.1.0"
     java
     idea
 }
